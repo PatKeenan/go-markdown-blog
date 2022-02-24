461aa096ac4367d0f7ebcaf575a16f6577c733f7
PatKeenan <patkeenan.dev@gmail.com>
2022-02-23 19:24:16 -0500

    switched to go mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module patkeenan.com/golang-blog
+
+go 1.17
+
+require github.com/russross/blackfriday v1.6.0
--- main.go
+++ main.go
@@ -1,23 +1,19 @@
-package main 
+package main
 
 import (
-    "fmt"
-    "os"
-    "log"
-    "net/http"
-    "github.com/gomarkdown/markdown"
+	"log"
+	"net/http"
+	"os"
+	"html/template"
+    "github.com/russross/blackfriday"
 )
 
 
 type Page struct {
     Title string
-    Body []byte
+    Body template.HTML
 }
 
-func (p *Page) save() error{
-    filename := p.Title + ".txt"
-    return os.WriteFile(filename, p.Body, 0600)
-}
 
 func loadPage(title string) *Page {
     filename := title + ".md"
@@ -25,37 +21,21 @@ func loadPage(title string) *Page {
     if err != nil {
         return nil 
     }
-    convertedBody := markdown.ToHTML(body, nil, nil)
-    return &Page{Title: title, Body: convertedBody}
-}
-
-func generator(){
- p1 := &Page{Title: "TestPage", Body:[]byte("This is a sample page.")}
- p1.save()
- p2 := loadPage("TestPage")
- fmt.Println(string(p2.Body))
+    //convertedToHtml := markdown.ToHTML(body, nil, nil)
+    newPost := template.HTML(blackfriday.MarkdownCommon([]byte(body)))
+    return &Page{Title: title, Body: newPost}
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request){
-    title := r.URL.Path[len("/view/"):]
+    title := r.URL.Path[len("/posts/"):]
     p := loadPage(title)
-    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
-}
-
-func editHandler (w http.ResponseWriter, r *http.Request){
-    title := r.URL.Path[len("/edit/"):]
-    p := loadPage(title)
-    fmt.Fprintf(w, "<h1>Editing %s</h1>"+
-        "<form action=\"/save/%s\" method=\"POST\">"+
-        "<textarea name=\"body\">%s</textarea><br>"+
-        "<input type=\"submit\" value=\"Save\">"+
-        "</form>",
-        p.Title, p.Title, p.Body)
-
+    t,_ := template.ParseFiles("./templates/posts.html")
+    t.Execute(w,p)
 }
 
 func main(){
-    http.HandleFunc("/view/", viewHandler)
-    http.HandleFunc("/edit/", editHandler)
+    http.HandleFunc("/posts/", viewHandler)
+    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
     log.Fatal(http.ListenAndServe(":3000", nil))
 }
